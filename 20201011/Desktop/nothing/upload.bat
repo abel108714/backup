f4c8a0409d13600dd7a4c7415657b063332696f7
unknown <udev77@mill.com.tw>
2020-10-11 23:30:35 +0800

    bak file

---
+++ 20201011/Desktop/nothing/upload.bat
@@ -0,0 +1,12 @@
+::將 heroku 新增到 git remote 當中
+::heroku git:remote -a test200629-linebot
+::初始化 git
+git init
+::提交所有檔案(.)
+git add .
+::提交到儲存庫
+git commit -am'ok'
+::遠端節點heroku的分支master上傳
+git push heroku master
+cls
+%comspec%
