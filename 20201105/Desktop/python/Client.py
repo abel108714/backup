0c0a9c99d6b788d4f10e1b20aa9dd312877bdac8
unknown <udev77@mill.com.tw>
2020-11-05 23:34:29 +0800

    bak file

---
+++ 20201105/Desktop/python/Client.py
@@ -0,0 +1,17 @@
+import os
+import socket
+
+HOST = '127.0.0.1'#'127.0.0.1''172.21.7.39'
+PORT = 8000
+clientMessage = 'Hello!'
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+client.sendall(clientMessage.encode())
+
+serverMessage = str(client.recv(1024), encoding='utf-8')
+print('Server:', serverMessage)
+
+client.close()
+
+os.system("pause")
