4fae8bf4537b0d827ad7c1f9b26d3b0a59ed11d4
unknown <udev77@mill.com.tw>
2020-11-07 23:32:03 +0800

    bak file

---
+++ 20201107/repos/WindowsApp2/WindowsApp1/Form2.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+	Inherits System.Windows.Forms.Form
+
+	'Form 覆寫 Dispose 以清除元件清單。
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.TextBox2 = New System.Windows.Forms.TextBox()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Button3 = New System.Windows.Forms.Button()
+		Me.SuspendLayout()
+		'
+		'TextBox1
+		'
+		Me.TextBox1.Location = New System.Drawing.Point(109, 85)
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(500, 22)
+		Me.TextBox1.TabIndex = 0
+		'
+		'Button1
+		'
+		Me.Button1.Location = New System.Drawing.Point(28, 83)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(75, 23)
+		Me.Button1.TabIndex = 1
+		Me.Button1.Text = "瀏覽"
+		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'Button2
+		'
+		Me.Button2.Location = New System.Drawing.Point(28, 151)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(75, 23)
+		Me.Button2.TabIndex = 5
+		Me.Button2.Text = "瀏覽"
+		Me.Button2.UseVisualStyleBackColor = True
+		'
+		'TextBox2
+		'
+		Me.TextBox2.Location = New System.Drawing.Point(109, 153)
+		Me.TextBox2.Name = "TextBox2"
+		Me.TextBox2.Size = New System.Drawing.Size(500, 22)
+		Me.TextBox2.TabIndex = 4
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(109, 67)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(118, 12)
+		Me.Label1.TabIndex = 6
+		Me.Label1.Text = "Excel 軟體安裝的路徑"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Location = New System.Drawing.Point(111, 135)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(77, 12)
+		Me.Label2.TabIndex = 7
+		Me.Label2.Text = "報表儲存路徑"
+		'
+		'Button3
+		'
+		Me.Button3.Location = New System.Drawing.Point(534, 211)
+		Me.Button3.Name = "Button3"
+		Me.Button3.Size = New System.Drawing.Size(75, 23)
+		Me.Button3.TabIndex = 8
+		Me.Button3.Text = "套用"
+		Me.Button3.UseVisualStyleBackColor = True
+		'
+		'Form2
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(636, 281)
+		Me.Controls.Add(Me.Button3)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.Button2)
+		Me.Controls.Add(Me.TextBox2)
+		Me.Controls.Add(Me.Button1)
+		Me.Controls.Add(Me.TextBox1)
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.Name = "Form2"
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "路徑設定"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents TextBox1 As TextBox
+	Friend WithEvents Button1 As Button
+	Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+	Friend WithEvents Button2 As Button
+	Friend WithEvents TextBox2 As TextBox
+	Friend WithEvents Label1 As Label
+	Friend WithEvents Label2 As Label
+	Friend WithEvents Button3 As Button
+End Class
