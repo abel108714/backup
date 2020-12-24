e8b2c65cd0f508bd8e67a211a647eb0992fbc60f
unknown <udev77@mill.com.tw>
2020-12-24 23:41:29 +0800

    bak file

--- 20201224/Desktop/TestWin.py
+++ 20201224/Desktop/TestWin.py
@@ -9,13 +9,13 @@ window.resizable(0,0) #不可以更改大⼩
 window.mainloop()
 
 
-import win32gui
-def is_screensaver_running():
+# import win32gui
+# def is_screensaver_running():
         #return win32gui.SystemParametersInfo(win32con.SPI_GETSCREENSAVERRUNNING) 
-        return win32gui.SystemParametersInfo(SPI_SETSCREENSAVERACTIVE, 0, Nothing, SPIF_SENDWININICHANGE)
+#         return win32gui.SystemParametersInfo(SPI_SETSCREENSAVERACTIVE, 0, Nothing, SPIF_SENDWININICHANGE)
 
 
-is_screensaver_running()
+# is_screensaver_running()
 
         # '關閉螢幕保護程式
         # SystemParametersInfo(SPI_SETSCREENSAVERACTIVE, 0, Nothing, SPIF_SENDWININICHANGE)
