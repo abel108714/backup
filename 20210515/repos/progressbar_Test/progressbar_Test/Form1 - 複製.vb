bc198cd3cdc01252cbc7dcc8d00e65a21d258e65
unknown <udev77@mill.com.tw>
2021-05-15 20:07:01 +0800

    bak file

---
+++ 20210515/repos/progressbar_Test/progressbar_Test/Form1 - 複製.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If ProgressBar1.Value <= ProgressBar1.Maximum - 1 Then
+            ProgressBar1.Value += 1
+        End If
+    End Sub
+End Class
