9d059b3ed274e25cf8510dff2f073ca51d8b6572
unknown <udev77@mill.com.tw>
2021-05-23 20:11:58 +0800

    bak file

---
+++ 20210523/repos/NewFileName/NewFileName/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Hide()
+        Me.Visible = False
+        Main()
+    End Sub
+    Private Sub Form1_Activated(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Activated
+        Me.Hide()
+        Me.Visible = False
+    End Sub
+    Public Shared Sub Main()
+        Console.WriteLine()
+        '  Invoke this sample with an arbitrary set of command line arguments.
+        Dim arguments As String() = Environment.GetCommandLineArgs()
+        Console.WriteLine("GetCommandLineArgs: {0}", String.Join(", ", arguments))
+    End Sub
+End Class
+
+Class Sample
+
+End Class
