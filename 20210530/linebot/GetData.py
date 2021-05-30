a29cc8e2c1c94c75d74f2b8af2d1cdbbffc6c4bc
unknown <udev77@mill.com.tw>
2021-05-30 20:12:03 +0800

    bak file

---
+++ 20210530/linebot/GetData.py
@@ -0,0 +1,9 @@
+﻿from openpyxl import Workbook
+import xlwings as xw
+path="S:\\開放公用區\★每日更新庫存水位警示表★\★庫存水位-唯讀參閱不可修改★.xlsm"
+wb = Workbook()
+wb = xw.Book(path)
+print(wb.sheets[0].range('A2').value)
+print(wb.sheets[0].range('B2').value)
+print(wb.sheets[0].range('C2').value)
+print(wb.sheets[0].range('D2').value)
