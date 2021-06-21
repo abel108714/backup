47d163e18cf9d84308a10e144b32422de44da021
unknown <udev77@mill.com.tw>
2021-06-21 20:07:48 +0800

    bak file

--- 20210610/linebot/heroku_git_remote.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-::將 heroku 新增到 git remote 當中
-heroku git:remote -a test200629-linebot
--- 20210614/Desktop/GetAMS.bat
+++
@@ -1,7 +0,0 @@
-@echo off 
-setlocal enabledelayedexpansion enableextensions
-
-wscript C:\auto\GetAMS.vbs S:\網通部\◎資訊\data\績效資料\Book1.xlsm
-
-
-exit
