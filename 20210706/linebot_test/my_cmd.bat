4818f2c0b269772ff7527f98c775be3a9c50552a
unknown <udev77@mill.com.tw>
2021-07-06 20:06:51 +0800

    bak file

---
+++ 20210706/linebot_test/my_cmd.bat
@@ -0,0 +1,14 @@
+@echo off
+set PATH=%PATH%;C:\Program Files\Heroku\bin;C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64;C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\Scripts;C:%HOMEPATH%\djangogirls\djangogirls_venv\Scripts;C:\Program Files\MySQL\MySQL Server 5.7\bin;C:\ngrok-stable-windows-amd64;C:\"Program Files (x86)"\Git\usr\bin
+
+echo %PATH%
+
+
+doskey act=C:%HOMEPATH%\djangogirls\djangogirls_venv\Scripts\activate
+
+
+set PATH=%PATH%;C:\linebot
+
+
+cls
+%comspec%
