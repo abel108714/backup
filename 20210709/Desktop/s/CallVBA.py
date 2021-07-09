dfe13177ae0a9ffc79a1e0bb4b7c4dc3583a0960
unknown <udev77@mill.com.tw>
2021-07-09 20:23:11 +0800

    bak file

---
+++ 20210709/Desktop/s/CallVBA.py
@@ -0,0 +1,21 @@
+﻿import os
+import win32com.client
+
+# 開啟 Excel
+excel = win32com.client.Dispatch("Excel.Application")
+
+# 開啟 hello.xlsm 活頁簿檔案
+excel.Workbooks.Open(Filename="S:\網通部\◎資訊\data\績效資料\Book1.xlsm")
+
+# 執行巨集程式
+excel.Application.Run("Book1.xlsm!INV.庫存水位")
+
+# 傳入參數，並取得計算結果
+#result = excel.Application.Run("hello.xlsm!hello2", "OfficeGuide.cc")
+#print(result)
+
+# 離開 Excel
+excel.Application.Quit()
+
+# 清理 com 介面
+del excel
