629218a4aa34207dce998b60abd833b2d2a51e88
unknown <udev77@mill.com.tw>
2021-07-26 20:05:41 +0800

    bak file

---
+++ 20210726/Desktop/python/mysql_test2.py
@@ -0,0 +1,24 @@
+﻿
+#import charts
+# 資料庫設定
+import mysql.connector
+
+mydb=mysql.connector.connect(
+    host= '127.21.7.39',
+    port= 3306,
+    user= 'root',
+    password= '16264386',
+    db= 'usersDB'
+)
+
+cur=mydb.cursor()
+sql="INSERT INTO users(UID,Dept,Name,checked) VALUES(%s,%s,%s,%s)"
+my_id=123#str(event.source.user_id)
+data=(my_id,'',str("周立修"),'')
+cur.execute(sql,data)
+mydb.commit()
+
+
+
+
+#INSERT INTO users(ID,Dept,Name,checked) VALUES('1','2','3','4')
---
+++ 20210726/repos/WindowsAppTest11/StateObject.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Threading
+Imports Microsoft.VisualBasic
+
+' State object for reading client data asynchronously
+
+Public Class StateObject
+    ' Client  socket.
+    Public workSocket As Socket = Nothing
+    ' Size of receive buffer.
+    Public Const BufferSize As Integer = 1024
+    ' Receive buffer.
+    Public buffer(BufferSize) As Byte
+    ' Received data string.
+    Public sb As New StringBuilder
+End Class 'StateObject
