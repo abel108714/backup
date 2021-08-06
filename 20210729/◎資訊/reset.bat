3428b8b42d3cc34a720d09160e9d89117132d279
unknown <udev77@mill.com.tw>
2021-08-06 14:21:05 +0800

    bak file

--- 20210729/◎資訊/reset.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-taskkill /f /im explorer.exe
-rd/s/q S:\網通部\◎資訊\test\Thumbs.db
-rd/s/q test
-md test
-start explorer.exe
-git clone git@github.com:abel108714/test.git
-
-pause
--- 20210731/Desktop/copy_GetAMS.bat
+++
@@ -1,2 +0,0 @@
-copy C:\Users\udev77\source\repos\GetAMS\GetAMS\bin\Debug\GetAMS.exe C:\auto\GetAMS.exe
-
