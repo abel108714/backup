6cfbc78a12d6088dfbe9fbc74eb172b2cd443781
unknown <udev77@mill.com.tw>
2021-08-27 20:06:20 +0800

    bak file

---
+++ 20210827/repos/GetPos/GetPos/Form1.vb
@@ -0,0 +1,198 @@
+﻿Imports System.Runtime.InteropServices
+
+'Imports System.IO
+'Imports System.Threading
+
+Public Class Form1
+
+    Private WithEvents kbHook As New KeyboardHook
+
+    '宣告API
+    Declare Function GetCursorPos Lib "user32" Alias "GetCursorPos" (ByRef lpPoint As POINTAPI) As Integer
+
+    Private Declare Function GetDC Lib "user32" (ByVal hwnd As Int32) As Int32
+    Private Declare Function ReleaseDC Lib "user32" (ByVal hwnd As Int32, ByVal hdc As Int32) As Int32
+    Private Declare Function GetPixel Lib "gdi32" (ByVal hdc As Int32, ByVal x As Int32, ByVal y As Int32) As Int32
+
+
+
+    'Private WithEvents kbHook As New KeyboardHook
+
+    'Private Declare Function BlockInput Lib "user32" _
+    '   (ByVal fBlock As Integer) As Integer
+    'Declare Auto Function SetCursorPos Lib "user32" (ByVal x As Integer, ByVal y As Integer) As Integer
+    'Declare Sub mouse_event Lib "user32" (ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal dwData As Integer, ByVal dwExtraInfo As Integer)
+
+    'Declare Function FindWindow Lib "user32" Alias "FindWindowA" (ByVal lpClassName As String, ByVal lpWindowName As String) As Integer
+    'Declare Function FindWindowEx Lib "user32" Alias "FindWindowExA" _
+    '(ByVal hWndParent As Integer, ByVal hWndChildAfter As Integer, ByVal lpClassName As String, ByVal lpWindowName As String) As Integer
+    'Private Declare Function SetForegroundWindow Lib "user32" (ByVal hwnd As IntPtr) As Long
+    'Private Declare Function GetForegroundWindow Lib "user32" () As IntPtr
+
+
+
+    Public Function Good_GetPixel(ByVal x As Int16, ByVal y As Int16) As Color
+        Dim kk As Int32 = GetPixel(GetDC(0), x, y)
+        Dim Color As Color = ColorTranslator.FromWin32(kk)
+        ReleaseDC(0, GetDC(0))
+        Return Color
+    End Function
+
+    Private Sub get_Color(ByVal x As Int16, ByVal y As Int16)
+        Dim c As Color = Good_GetPixel(x, y)   'Return a color
+        'MsgBox("RGB = " & c.R & "." & c.G & "." & c.B)
+        Label2.Text = "RGB = " & c.R & "." & c.G & "." & c.B
+        'Label1.BackColor = c    '某個標籤的背景顏色變換指定顏色
+    End Sub
+
+    Structure POINTAPI
+        Dim x As Integer
+        Dim y As Integer
+    End Structure
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.TopMost = True
+        Timer1.Enabled = True
+        Timer1.Interval = 50 '每0.05秒執行一次
+
+        'Me.KeyPreview = True
+
+    End Sub
+
+    Public now_coord As String = ""
+    Dim p As POINTAPI
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        GetCursorPos(p) '取得目前座標
+        now_coord = "p.x = " & p.x & " p.y = " & p.y
+        If Label1.Text <> now_coord Then '比對座標是否不同，不同就更新Label1
+            Label1.Text = now_coord
+            'Dim NewPt As ScrnPt = New ScrnPt(1280, 1024, 1366, 768)
+            'get_Color(NewPt.getPosX(790), NewPt.getPosY(497))
+            Dim NewPt As ScrnPt = New ScrnPt(1280, 1024, 1366, 768)
+            get_Color(NewPt.getPosX(p.x), NewPt.getPosY(p.y))
+            Label1.Refresh()
+            Label2.Refresh()
+        End If
+    End Sub
+
+
+    Private Sub kbHook_KeyDown(ByVal Key As System.Windows.Forms.Keys) Handles kbHook.KeyDown
+        'MsgBox(Key)
+        If Key = 112 Or 192 Then 'F1
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub kbHook_KeyUp(ByVal Key As System.Windows.Forms.Keys) Handles kbHook.KeyUp
+        'MsgBox(Key)
+    End Sub
+
+End Class
+
+Public Class KeyboardHook
+
+    <DllImport("User32.dll", CharSet:=CharSet.Auto, CallingConvention:=CallingConvention.StdCall)>
+    Private Overloads Shared Function SetWindowsHookEx(ByVal idHook As Integer, ByVal HookProc As KBDLLHookProc, ByVal hInstance As IntPtr, ByVal wParam As Integer) As Integer
+    End Function
+    <DllImport("User32.dll", CharSet:=CharSet.Auto, CallingConvention:=CallingConvention.StdCall)>
+    Private Overloads Shared Function CallNextHookEx(ByVal idHook As Integer, ByVal nCode As Integer, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Integer
+    End Function
+    <DllImport("User32.dll", CharSet:=CharSet.Auto, CallingConvention:=CallingConvention.StdCall)>
+    Private Overloads Shared Function UnhookWindowsHookEx(ByVal idHook As Integer) As Boolean
+    End Function
+
+    <StructLayout(LayoutKind.Sequential)>
+    Private Structure KBDLLHOOKSTRUCT
+        Public vkCode As UInt32
+        Public scanCode As UInt32
+        Public flags As KBDLLHOOKSTRUCTFlags
+        Public time As UInt32
+        Public dwExtraInfo As UIntPtr
+    End Structure
+
+    <Flags()>
+    Private Enum KBDLLHOOKSTRUCTFlags As UInt32
+        LLKHF_EXTENDED = &H1
+        LLKHF_INJECTED = &H10
+        LLKHF_ALTDOWN = &H20
+        LLKHF_UP = &H80
+    End Enum
+
+    Public Shared Event KeyDown(ByVal Key As Keys)
+    Public Shared Event KeyUp(ByVal Key As Keys)
+
+    Private Const WH_KEYBOARD_LL As Integer = 13
+    Private Const HC_ACTION As Integer = 0
+    Private Const WM_KEYDOWN = &H100
+    Private Const WM_KEYUP = &H101
+    Private Const WM_SYSKEYDOWN = &H104
+    Private Const WM_SYSKEYUP = &H105
+
+    Private Delegate Function KBDLLHookProc(ByVal nCode As Integer, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Integer
+
+    Private KBDLLHookProcDelegate As KBDLLHookProc = New KBDLLHookProc(AddressOf KeyboardProc)
+    Private HHookID As IntPtr = IntPtr.Zero
+
+    Private Function KeyboardProc(ByVal nCode As Integer, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Integer
+        If (nCode = HC_ACTION) Then
+            Dim struct As KBDLLHOOKSTRUCT
+            Select Case wParam
+                Case WM_KEYDOWN, WM_SYSKEYDOWN
+                    RaiseEvent KeyDown(CType(CType(Marshal.PtrToStructure(lParam, struct.GetType()), KBDLLHOOKSTRUCT).vkCode, Keys))
+                Case WM_KEYUP, WM_SYSKEYUP
+                    RaiseEvent KeyUp(CType(CType(Marshal.PtrToStructure(lParam, struct.GetType()), KBDLLHOOKSTRUCT).vkCode, Keys))
+            End Select
+        End If
+        Return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam)
+    End Function
+
+    Public Sub New()
+        HHookID = SetWindowsHookEx(WH_KEYBOARD_LL, KBDLLHookProcDelegate, System.Runtime.InteropServices.Marshal.GetHINSTANCE(System.Reflection.Assembly.GetExecutingAssembly.GetModules()(0)).ToInt32, 0)
+        If HHookID = IntPtr.Zero Then
+            Throw New Exception("Could not set keyboard hook")
+        End If
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        If Not HHookID = IntPtr.Zero Then
+            UnhookWindowsHookEx(HHookID)
+        End If
+        MyBase.Finalize()
+    End Sub
+
+End Class
+
+
+Public Class ScrnPt
+
+    Dim OrigSpecX As Integer = 0
+    Dim OrigCtrX As Integer = 0
+    Dim OrigSpecY As Integer = 0
+    Dim OrigCtrY As Integer = 0
+    Dim NewCtrX As Integer = 0
+    Dim NewCtrY As Integer = 0
+    Public NewSpecX As Integer = 0
+    Public NewSpecY As Integer = 0
+
+    Public Sub New(OrigSizeX As Integer, OrigSizeY As Integer, NewSizeX As Integer, NewSizeY As Integer)
+        '原螢幕中心點
+        Me.OrigCtrX = OrigSizeX / 2
+        Me.OrigCtrY = OrigSizeY / 2
+        '新螢幕中心點
+        Me.NewCtrX = NewSizeX / 2
+        Me.NewCtrY = NewSizeY / 2
+    End Sub
+
+    Public Function getPosX(OrigSpecX As Integer)
+        Me.NewSpecX = Me.NewCtrX - (Me.OrigCtrX - OrigSpecX)
+        Return Me.NewSpecX
+    End Function
+
+    Public Function getPosY(OrigSpecY As Integer)
+        Me.NewSpecY = Me.NewCtrY - (Me.OrigCtrY - OrigSpecY)
+        Return Me.NewSpecY
+    End Function
+
+End Class
+
