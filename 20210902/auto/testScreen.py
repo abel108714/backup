423f8184f121b06c1c36bed327a1aa13fff2c489
unknown <udev77@mill.com.tw>
2021-09-04 14:03:39 +0800

    bak file

--- 20210902/auto/testScreen.py
+++
@@ -1,9 +0,0 @@
-import win32gui
-import win32con
-
-#to turn off use :-
-win32gui.SendMessage(win32con.HWND_BROADCAST,
-                     win32con.WM_SYSCOMMAND, win32con.SC_MONITORPOWER, 2)
-#turn on use :-
-win32gui.SendMessage(win32con.HWND_BROADCAST,
-                     win32con.WM_SYSCOMMAND, win32con.SC_MONITORPOWER, -1)
