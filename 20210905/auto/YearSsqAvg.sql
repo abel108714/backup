58583164c01ce9c0deabeb074ce8d4b5a480c792
unknown <udev77@mill.com.tw>
2021-09-05 20:05:44 +0800

    bak file

---
+++ 20210905/auto/YearSsqAvg.sql
@@ -0,0 +1,13 @@
+﻿DELIMITER ;;
+DROP PROCEDURE IF EXISTS getYearSsqAvg;
+CREATE PROCEDURE getYearSsqAvg(id VARCHAR(255))
+BEGIN
+
+	call getYearSsqSum(id,@ContactType);
+	select @ContactType/12;
+
+END;
+;;
+DELIMITER ;
+
+
