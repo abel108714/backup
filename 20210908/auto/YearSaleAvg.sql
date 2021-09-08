bd7e11136da51b4b4632295b34a4ab7e55339153
unknown <udev77@mill.com.tw>
2021-09-08 20:06:51 +0800

    bak file

---
+++ 20210908/auto/YearSaleAvg.sql
@@ -0,0 +1,16 @@
+﻿DELIMITER ;;
+DROP PROCEDURE IF EXISTS getYearSaleAvg;
+CREATE PROCEDURE getYearSaleAvg(IN id VARCHAR(255))
+BEGIN
+
+	call getYearSqSum(id,@YearSqSum);
+	call getYearSsqSum(id,@YearSsqSum);
+	select @YearSqSum;
+	select @YearSsqSum;
+	select (@YearSqSum+@YearSsqSum)/12;
+
+END;
+;;
+DELIMITER ;
+
+
