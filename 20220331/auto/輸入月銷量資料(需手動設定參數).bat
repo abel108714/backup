990243e40626e0c7fa8429462d623fb5b9bfe80e
unknown <udev77@mill.com.tw>
2022-04-01 00:16:58 +0800

    bak file

---
+++ 20220331/auto/輸入月銷量資料(需手動設定參數).bat
@@ -0,0 +1,11 @@
+@echo off 
+
+start /w GetAllSSQ.py 2021 07
+start /w GetAllSSQ.py 2021 08
+start /w GetAllSSQ.py 2021 09
+start /w GetAllSSQ.py 2021 10
+start /w GetAllSSQ.py 2021 11
+start /w GetAllSSQ.py 2021 12
+
+
+pause
