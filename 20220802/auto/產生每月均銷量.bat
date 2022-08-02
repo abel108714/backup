cdeb85ceca299e722a2e3c6a361da262ec202552
unknown <udev77@mill.com.tw>
2022-08-02 20:06:55 +0800

    bak file

---
+++ 20220802/auto/產生每月均銷量.bat
@@ -0,0 +1,16 @@
+@echo off 
+
+::取鼎新報表
+::start /wait /min C:\auto\GetAMS.exe
+
+::計算每月均銷量寫入excel
+::wscript C:\auto\GetAMS.vbs S:\網通部\◎資訊\data\績效資料\Book1.xlsm
+
+::start /wait /min C:\auto\GetSQ2.py
+
+
+start /min C:\auto\GetAMS.bat
+
+
+
+exit
